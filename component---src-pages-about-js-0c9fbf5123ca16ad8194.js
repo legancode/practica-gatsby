66c9e24035f50adda5c3c1eda9855ece7ccc7fb4
Angel Salvador <angelsalvadordev@gmail.com>
2019-10-05 23:45:58 -0500

    Updates

---
+++ component---src-pages-about-js-0c9fbf5123ca16ad8194.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{191:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=function(e){var t=e.headerText;return r.a.createElement("h1",null,t)};t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c,{headerText:"Sobre Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-0c9fbf5123ca16ad8194.js.map
